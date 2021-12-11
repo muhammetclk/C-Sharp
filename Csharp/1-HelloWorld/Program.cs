1bdd6134d54cb9d57389e39a725932a48d4ce20a
Muhammet Çelik <muhammetclk56@gmail.com>
2021-12-11 15:07:54 +0300

    Hello World yazdirildi.

--- Csharp/1-HelloWorld/Program.cs
+++ Csharp/1-HelloWorld/Program.cs
@@ -10,6 +10,18 @@ namespace _1_HelloWorld
     {
         static void Main(string[] args)
         {
+            //Console sinifina ait WriteLine fonksiyonu ile yazdirma islemi yapariz.
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Muhammet Celik");
+
+            //Console sinifina ait Write fonksiyonu WriteLine ile ayni islemi yapar Fakat yanyana ve bitisik yazar. 
+            Console.Write("ahmet");
+            Console.Write("mehmet");
+
+            /*Console sinifina ait REadLine fonksiyonu acilan konsol ekraninin sabit kalmasini saglar.*/
+            Console.ReadLine();
+
+
         }
     }
 }
