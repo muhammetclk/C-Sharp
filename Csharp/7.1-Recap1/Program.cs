527615f2720132b297ad19dd1c57c7b9b06d699a
Muhammet Çelik <muhammetclk56@gmail.com>
2021-12-13 14:57:40 +0300

    Ogrendigimiz konularla alakali ornek calismalar yapildi.

---
+++ Csharp/7.1-Recap1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Recap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanici giris sayfasi uygulamasi.
+            //kullanici adi ve sifresini dogru girerse giris yaptiniz yazsin
+            //3`den fazla yanlis girerse hesabiniz bloke olmus yazsin.
+            int hak = 3;
+            string kullaniciadi = "Muhammet";
+            string sifre = "123456";
+            while (true)
+            {
+
+                Console.Write("Lutfen Kullanici Adini Giriniz:");
+                string giriskullaniciadi = Console.ReadLine();
+                Console.Write("Lutfen Sifrenizi Giriniz:");
+                string girissifre = Console.ReadLine();
+                if (kullaniciadi == giriskullaniciadi && sifre == girissifre)
+                {
+                    Console.WriteLine("Giris Yaptiniz...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanici adi veya Sifreniz yanlis ");
+                    if (hak == 0)
+                    {
+                        Console.WriteLine("Hesabiniz Bloke olmustur.");
+                        break;
+                    }
+                    else
+                        hak--;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp/7.2-Recap2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_Recap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanicidan alinan sayiya kadar olan degerleri toplayip ortalamasini bulan program
+            //ortalama 50`den buyukse ortalama 50`den buyuktur degilse 50`den kucuktur yazsin.
+
+            Console.Write("Girdiginiz sayiya kadar toplayip Ortalamasi alinacak sayiyi giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            double toplam = 0;
+
+            int i = 0;
+            for (; i <= sayi; i++)
+            {
+                toplam += i;
+            }
+            double ortalama = toplam / sayi;
+
+            if (ortalama > 50)
+            {
+                Console.WriteLine("Ortalama 50`den buyuktur.");
+                Console.WriteLine("Ortalama" + ortalama);
+            }
+            else
+            {
+                Console.WriteLine("Ortalama 50`den kucuktur.");
+                Console.WriteLine("Ortalama:" + ortalama);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
