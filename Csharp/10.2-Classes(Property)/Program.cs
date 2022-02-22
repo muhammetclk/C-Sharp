6895be49a503137ed6fec7bceab45384332c5db3
Muhammet Çelik <muhammetclk56@gmail.com>
2022-02-22 22:25:27 +0300

    Classlar ve property ile ilgili calismalar yapildi.

---
+++ Csharp/10.2-Classes(Property)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._2_Classes_Property_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;                              //deger atamalarini yaptik.
+            customer1.FirstName = "Muhammet";
+            customer1.LastName = "Celik";
+            customer1.City = "Istanbul";
+            Console.WriteLine("cutomer1:" + (customer1.FirstName + customer1.LastName));
+
+            Customer customer2 = new Customer { Id = 2, FirstName = "Enver", LastName = "Celik", City = "Istanbul" };//deger atamasini bu sekildede yapabiliriz.
+            Console.WriteLine("customer2:" + (customer2.FirstName + customer2.LastName));
+            Console.ReadLine();
+        }
+    }
+    class Customer
+    {
+        public int Id { get; set; } //get ve set bloklari kapali oldugu icin normal degisken gibi davranir.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Csharp/10.3-Classes(Property)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._3_Classes_Property_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Muhammet";
+            Console.WriteLine("FirstName:" + customer.FirstName);
+            Console.ReadLine();
+        }
+    }
+    class Customer
+    {
+        private string _FirstName;
+
+        public string FirstName //property tanimladik ve set bloguyla private degiskenimize deger atadik. get bloguyla atadigimiz degeri dondurduk.
+        {
+            get
+            {
+                return _FirstName;
+            }
+            set
+            {
+                _FirstName = value;
+            }
+        }
+        public string LastName { get; set; }
+
+
+    }
+}
