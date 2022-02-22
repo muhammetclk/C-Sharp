ac50b133602b1ff19a0a678fceded0e93e51389c
Muhammet Çelik <muhammetclk56@gmail.com>
2022-02-22 22:50:24 +0300

    Inheritance ile ilgili calismalar yapildi.

---
+++ Csharp/13.1-Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._1_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[3]//Person classindan da nesne olusturabiliriz.
+            {
+                new Customer{FirstName="Muhammet",LastName="Celik",Id=3,City="Istanbul" },
+                new Student{FirstName="Enver",LastName="Celik",Id=2,Department="Computer Sciences" },
+                new Person{FirstName="Firat",LastName="Celik",Id=1}
+            };
+            foreach (var item in person)
+            {
+                Console.WriteLine(item.FirstName);
+                Console.WriteLine(item.LastName);
+                Console.WriteLine(item.Id);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+    }
+    class Customer : Person //Person classindan miras aliyor.(Personda tanimladigimiz ozellikleri icine yazmadan  kullanabiliriz.)
+    {
+        public string City { get; set; } //ekstra ozelliklerde tanimlayabiliriz.
+    }
+    class Student : Person //classlar sadece bir tane miras alir.
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ Csharp/13.2-Inheritance(Virtual Method)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._2_Inheritance_Virtual_Method_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            sqlServer.Delete();
+            MySql mySql = new MySql();
+            mySql.Add();
+            mySql.Delete();
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public virtual void Add() //Metodu virtual tanimlayip miras alinan yerde override edersek oradaki kod calisir.
+        {
+            Console.WriteLine("Added by default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by sql code");
+            // base.Add();//Miras alinan yani default metodu calistirir.
+        }
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by sql code");
+            //base.Delete();
+        }
+    }
+    class MySql : Database //override etmedigimiz icin temel kod calisir. temel kod calisir
+    {
+
+    }
+}
