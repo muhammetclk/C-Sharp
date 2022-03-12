5838709de55814044092bebb1e1c3b11f30e4a57
Muhammet Çelik <muhammetclk56@gmail.com>
2022-03-12 15:21:41 +0300

    Exceptions ile calisma yapildi.

---
+++ Csharp/20.1-Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._1_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string[] cities = new string[3] { "Adana", "istanbul", "izmir" };
+                cities[3] = "ankara"; //3. indeks olmadigindan hata olur.(ve catch kismina gecer.)
+
+
+            }
+            catch (IndexOutOfRangeException exception) //index siniri hatasi verince bu catch girer.(sadece catch yazip parametresizde olusturabilirdik.)
+            {
+                Console.WriteLine(exception.Message);
+
+            }
+            catch (Exception exception)//diger hatalarda bu catcha girer.(bu sekilde birden fazla catch olusturabiliriz.)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
